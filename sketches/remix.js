0c3e95b7f782df609bf063f1e72c8888e47b77e4
lanaalsaleh <vn25xf@brocku.ca>
2026-05-29 00:03:33 -0400

    added partciles from sketch 1

--- sketches/remix.js
+++ sketches/remix.js
@@ -3,12 +3,22 @@
 // This is where sketch1 and sketch2 come together
 // into something new
 // =============================================
-
+ // your combined code here
+var pix = [];
 function setup() {
+  // i copied the particle system from sketch 1 
+  // wanted to start building the remix with the moving dots
   createCanvas(800, 500);
+for (var i = 0; i < width / 10; i++) {
+    for (var j = 0; j < height / 20; j++) {
+      pix[i * (height / 20) + j] = 
+      new Pixel(i * 10, j * 10, i / 100.0 * j / 100 * 8.0, true);
+    }
+  }
 }
 
 function draw() {
   background(220);
-  // your combined code here
+ 
 }
+
